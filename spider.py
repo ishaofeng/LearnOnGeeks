e2803a269f3dd3db135435bb0d206a60642703e9
fakir <ishaofeng@foxmail.com>
2014-10-29 21:29:11 +0800

    add spider

---
+++ spider.py
@@ -0,0 +1,63 @@
+#! /usr/bin/python
+#encoding=utf-8
+
+from __future__ import unicode_literals
+
+from pyquery import PyQuery as pq
+import json
+
+baseurl = "http://www.geeksforgeeks.org/"
+urls = [
+#("data-structures", True)
+("c-plus-plus", False)
+]
+
+def parserCatagery(info):
+    '''
+        解析一个题目页面，解析生成的题目结构
+    '''
+    result = {}
+
+    doc = pq(url=(baseurl + info[0]))
+
+    #分类名称
+    title = doc("h2.page-title").html()
+    result["title"] = title
+    result["type"] = info[1]
+    result["sections"] = {}
+
+    #分类解析处理
+    for p in doc("div.page-content > p").items():
+        sectionname = p("strong").html()
+        section = {}
+
+        em = {}
+        emname = ""
+        for item in p.children().items():
+            if item.is_("em"):
+                if em:
+                    section[emname] = em
+
+                emname = item.text()
+                em = {}
+            elif item.is_("a"):
+                em[item.html()] = item.attr.href
+
+        if result["type"] == True:
+            if em:
+                section[emname] = em
+        else:
+            section = em
+
+        result["sections"][sectionname] = section
+
+    return result
+
+
+if __name__ == "__main__":
+    for url in urls:
+        result = parserCatagery(url)
+
+        with open("data.json", "w") as f:
+            json.dump(result, f, indent=4)
+
